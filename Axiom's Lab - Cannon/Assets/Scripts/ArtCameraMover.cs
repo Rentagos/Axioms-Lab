a2cf4e1f90cfe0ad812f99de7b0da22cb8ec51af
Rentagos <37207246+Rentagos@users.noreply.github.com>
2019-10-28 17:05:54 -0400

    Pushing V1

---
+++ Axiom's Lab - Cannon/Assets/Scripts/ArtCameraMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtCameraMover : MonoBehaviour
+{
+
+    public float speed;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.localPosition += transform.forward * speed;
+        if (Input.GetKey(KeyCode.S))
+            transform.localPosition -= transform.forward * speed;
+        if (Input.GetKey(KeyCode.A))
+            transform.localPosition -= transform.right * speed;
+        if (Input.GetKey(KeyCode.D))
+            transform.localPosition += transform.right * speed;
+    }
+}
