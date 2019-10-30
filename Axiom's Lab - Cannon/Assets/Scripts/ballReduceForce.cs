190a3b579205704cb6639dd7ed78ef6875b6e1e4
Rentagos <37207246+Rentagos@users.noreply.github.com>
2019-10-30 17:25:59 -0400

    Worked on the Physics System

---
+++ Axiom's Lab - Cannon/Assets/Scripts/ballReduceForce.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballReduceForce : MonoBehaviour
+{
+    public float reduceX = 0;
+    public float reduceY = 0;
+    public float reduceZ = 0;
+
+    public float speed = 1.0f;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<ConstantForce>().force = new Vector3(reduceX, reduceY, reduceZ);
+
+        if (reduceX > 0)
+        {
+            reduceX -= Time.deltaTime * 200;
+        }
+
+        if (reduceX < 0)
+        {
+            reduceX = 0;
+        }
+
+        if (reduceY > 0)
+        {
+            reduceY -= Time.deltaTime * 200;
+        }
+
+        if (reduceY < 0)
+        {
+            reduceY = 0;
+        }
+
+        if (reduceZ > 0)
+        {
+            reduceZ -= Time.deltaTime * 200;
+        }
+
+        if (reduceZ < 0)
+        {
+            reduceZ = 0;
+        }
+
+
+    }
+
+}
---
+++ Axiom's Lab - Cannon/Assets/Scripts/cannonScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cannonScript : MonoBehaviour
+{
+
+    public float rForceX = 0;
+    public float rForceY = 0;
+    public float rForceZ = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ball"))
+        {
+
+            other.gameObject.GetComponent<ConstantForce>().force = new Vector3(rForceX, rForceY, rForceZ);
+
+        }
+    }
+
+    public void sliderX(Slider slider)
+    {
+        rForceX = slider.value;
+    }
+
+    public void sliderY(Slider slider)
+    {
+        rForceY = slider.value;
+    }
+
+    public void sliderZ(Slider slider)
+    {
+        rForceZ = slider.value;
+    }
+}
